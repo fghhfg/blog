8c3d375de51f817cafe5937555cfeb718f723425
wybcp <wangyb65@gmail.com>
2014-06-02 23:16:01 +0800

    part three:mongodb

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,38 @@
  */
 module.exports = function(app){
 	app.get('/',function(req, res){
-  	res.render('index', { title: 'Express' });//使用 ejs 模板引擎解析 views/index.ejs
+  	res.render('index', { title: '主页' });//使用 ejs 模板引擎解析 views/index.ejs
 		}
-	)
+	);
+	
+	app.get('/reg',function(req, res){
+  	res.render('reg', { title: '注册' });
+		}
+	);
+	app.post('/reg',function(req,res){
+	
+	});
+	
+	app.get('/login',function(req, res){
+  	res.render('login', { title: '登陆' });
+		}
+	);
+	app.post('/login',function(req,res){
+	
+	});
+	
+	app.get('/post',function(req, res){
+  	res.render('post', { title: '发表' });
+		}
+	);
+	app.post('/post',function(req,res){
+	
+	});
+	
+	app.get('/logout',function(req, res){
+  	
+		});
+	
+	
 };
 
