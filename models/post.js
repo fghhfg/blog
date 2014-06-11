00798555f959e4fcc593337fa4ce2e17d14c71f4
wybcp <wangyb65@gmail.com>
2014-06-11 20:54:43 +0800

    part :edit and remove

--- models/post.js
+++ models/post.js
@@ -119,3 +119,100 @@ Post.getOne = function(name, day,title, callback){
 		})
 	})
 }
+
+//修改文章
+Post.edit = function(name,day,title,callback){
+	//打开数据库
+	mongodb.open(function(err,db){
+		if(err){
+			return callback(err);
+		}
+		
+		db.collection('posts',function(err,collection){
+			if(err){
+				mongodb.close();
+				return callback(err);
+			}
+			
+			//根据day,name,title对象查询文章
+			collection.findOne({
+				"name": name,
+				"time.day":day,
+				"title":title
+			},function(err,doc){
+					mongodb.close();
+					if(err){
+						return callback(err);
+					}
+					callback(null,doc);//成功，已数组形式返回查询结果
+			})
+		})
+	})
+}
+
+//更新文章
+Post.update = function(name,day,title,post,callback){
+	//打开数据库
+	mongodb.open(function(err,db){
+		if(err){
+			return callback(err);
+		}
+		
+		db.collection('posts',function(err,collection){
+			if(err){
+				mongodb.close();
+				return callback(err);
+			}
+			
+			//更新文章
+			collection.update({
+				"name": name,
+				"time.day":day,
+				"title":title
+			},{
+				$set: {post: post}
+			},function(err){
+					mongodb.close();
+					if(err){
+						return callback(err);
+					}
+					callback(null);
+			})
+		})
+	})
+}
+
+
+//删除文章
+Post.remove = function(name,day,title,callback){
+	//打开数据库
+	mongodb.open(function(err,db){
+		if(err){
+			return callback(err);
+		}
+		
+		db.collection('posts',function(err,collection){
+			if(err){
+				mongodb.close();
+				return callback(err);
+			}
+			
+			//查找并删除文章
+			collection.remove({
+				"name": name,
+				"time.day":day,
+				"title":title
+			},{
+				w: 1
+			},function(err){
+					mongodb.close();
+					if(err){
+						return callback(err);
+					}
+					callback(null);
+			})
+		})
+	})
+}
+
+
