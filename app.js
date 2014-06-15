b31b05c4e41d2f43807db273888a143db242fc72
wybcp <wangyb65@gmail.com>
2014-06-15 21:41:46 +0800

    part:page and archive

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 
-/**
- * Module dependencies.
- */
+//Module dependencies
 
 var express = require('express');
 var routes = require('./routes');
-var user = require('./routes/user');
+var fs = require('fs');
 var http = require('http');
 var path = require('path');
 var MongoStore = require('connect-mongo')(express);
@@ -17,7 +15,7 @@ var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 8000);
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 
 app.use(flash());//flash通知中间件
@@ -28,14 +26,16 @@ app.use(express.urlencoded());
 app.use(express.multipart({keepExtensions: true,uploadDir:'./public/images'}));//上传文件,保留文件后缀名，上传目录设置
 app.use(express.methodOverride());//connect 内建的中间件，可以协助处理 POST 请求，伪装 PUT、DELETE 和其他 HTTP 方法
 app.use(express.cookieParser());//cookie解析
+
 app.use(express.session({
 	secret: settings.cookieSecret,//防止篡改cookie
 	key: settings.db,//cookie name
 	cookie: {maxAge: 1000*60*60*24*30},//30 days
-	store:new MongoStore({
-		db:settings.db
+	store: new MongoStore({
+		db: settings.db
 	})//把会话信息存储在数据库
-	}));//session surport
+}));//session surport
+
 app.use(app.router);//调解路由解析规则
 app.use(express.static(path.join(__dirname, 'public')));//设置静态文件目录
 
@@ -44,8 +44,6 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }//开发环境的错误处理，输出错误信息
 //路由控制器
-
-app.get('/users', user.list);
 
 routes(app);//总的路由接口
 
